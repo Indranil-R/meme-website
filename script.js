f08e5c965e30d404186d96d2268fad8d363c91ae
Indranil Roy <87275536+Indranil-R@users.noreply.github.com>
2022-02-16 03:33:32 +0530

    uodate

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-
-
 var apiUrl = "https://meme-api.herokuapp.com/gimme";
-var image = document.getElementById('meme-img');
-
-
 
 var loadMeme = async () => {
-    image.innerHTML = '<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">    <span class="sr-only">Loading...</span>  </div>';
-    var memeLink = await fetch(apiUrl).catch(error);
-    memeLink = await memeLink.json();
-    var memeUrl = memeLink.preview.slice(-1);
-    var meme = document.createElement('img');
-    meme.src = memeUrl;
+    try {
+        var image = document.getElementById("imageBody");
+        image.innerHTML = '<div class="spinner-border" style="width: 3rem; height: 3rem;" role="status">    <span class="sr-only">Loading...</span>  </div>';
+        var memeLink = await fetch(apiUrl).catch(errorHandle);
+        memeLink = await memeLink.json();
+        var memeUrl = memeLink.preview.slice(-1);
+        addMeme(memeUrl);
+        addDownloadBtn(memeUrl);
+    } catch (error) {
+        errorHandle();
+    }
+};
+
+errorHandle = () => {
+    document.write("Err : 500<br>Something went wrong Please refresh the page ");
+};
+
+function addMeme(url) {
+    var image = document.getElementById("imageBody");
+    var meme = document.createElement("img");
+    meme.src = url;
     meme.classList = "img-fluid";
     image.innerHTML = null;
     image.append(meme);
 }
 
-error = () => {
-    document.write("error Occured")
+function addDownloadBtn(url) {
+    let downloadBtn = document.createElement("a");
+    let downloadBtnDiv = document.getElementById("downloadBtnDiv");
+
+    downloadBtn.setAttribute("href", url);
+    downloadBtn.setAttribute("download", "meme");
+    downloadBtn.classList = "btn btn-primary text-center";
+    downloadBtn.innerText = "Download";
+
+    downloadBtnDiv.innerHTML = "";
+    downloadBtnDiv.append(downloadBtn);
 }
